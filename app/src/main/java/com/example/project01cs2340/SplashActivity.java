d173b17ead818638bf4c365f9aa51af17d05e0dd
paulaelaigwu <paulaelaigwu@gmail.com>
2024-01-22 23:07:51 -0500

    Finished Part 2 of the ToDo Tutorial

---
+++ app/src/main/java/com/example/project01cs2340/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.project01cs2340;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
