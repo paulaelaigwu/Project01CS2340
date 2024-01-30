6afa1f9b543b97863581c251836c1c94a97be7e7
paulaelaigwu <paulaelaigwu@gmail.com>
2024-01-30 13:07:44 -0500

    ...

--- app/src/main/java/com/example/project01cs2340/ItemFragment.java
+++ app/src/main/java/com/example/project01cs2340/ItemFragment.java
@@ -53,7 +53,7 @@ public class ItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_item, container, false);
+        View view = inflater.inflate(R.layout.fragment_blank2, container, false);
 
         // Set the adapter
         if (view instanceof RecyclerView) {
