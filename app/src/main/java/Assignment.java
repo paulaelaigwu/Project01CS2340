44178e4f23d697ebf4edf9288abe687a1f6f0df4
aminaaeld <amina.eldahshoury03@gmail.com>
2024-02-01 17:11:49 -0500

    opening up the buttons to activity

---
+++ app/src/main/java/Assignment.java
@@ -0,0 +1,12 @@
+public class Assignment {
+    private String CourseID;
+
+    public Assignment(String courseID, String courseName) {
+        CourseID = courseID;
+        CourseName = courseName;
+    }
+
+    private String CourseName;
+
+
+}
