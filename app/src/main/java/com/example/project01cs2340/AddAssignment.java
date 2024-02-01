a010db977310f2273259c3cca56dd3c75659cf82
aminaaeld <amina.eldahshoury03@gmail.com>
2024-02-01 17:01:08 -0500

    layout for assignment popup page for user input

---
+++ app/src/main/java/com/example/project01cs2340/AddAssignment.java
@@ -0,0 +1,61 @@
+package com.example.project01cs2340;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class AddAssignment extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private TextView DueDate;
+
+    Button btn_Save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_assignment);
+        DueDate = findViewById(R.id.btn_DueDate);
+
+        findViewById(R.id.et_DateDue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+        btn_Save = findViewById(R.id.btn_Save);
+
+        btn_Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddAssignment.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                );
+        datePickerDialog.show();
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = "month/day/year" + (month+1) "/" + dayOfMonth + "/" + year;
+        DueDate.setText(date);
+
+    }
+}
