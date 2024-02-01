205433c7de76cd74cbe9f6322f752fb0cc6d8bbd
aminaaeld <amina.eldahshoury03@gmail.com>
2024-02-01 17:17:07 -0500

    getters and setters

--- app/src/main/java/Assignment.java
+++ app/src/main/java/Assignment.java
@@ -1,12 +1,33 @@
 public class Assignment {
     private String CourseID;
+    private String CourseName;
 
     public Assignment(String courseID, String courseName) {
         CourseID = courseID;
         CourseName = courseName;
     }
 
-    private String CourseName;
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "CourseID='" + CourseID + '\'' +
+                ", CourseName='" + CourseName + '\'' +
+                '}';
+    }
 
+    public String getCourseID() {
+        return CourseID;
+    }
 
+    public void setCourseID(String courseID) {
+        CourseID = courseID;
+    }
+
+    public String getCourseName() {
+        return CourseName;
+    }
+
+    public void setCourseName(String courseName) {
+        CourseName = courseName;
+    }
 }
